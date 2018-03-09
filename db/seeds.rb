334c4d293552fc1e62b05e0719855fce1e608252
Stephanie Simpson <stephsimpson911@gmail.com>
2018-03-09 23:45:41 +0000

    added nested payee in expenses

--- db/seeds.rb
+++ db/seeds.rb
@@ -64,3 +64,81 @@ attendee5 = Attendee.create!([ { user_id: 2, trip_id: 2 }])
 attendee5 = Attendee.create!([ { user_id: 3, trip_id: 1 }])
 attendee5 = Attendee.create!([ { user_id: 3, trip_id: 3 }])
 attendee5 = Attendee.create!([ { user_id: 3, trip_id: 2 }])
+
+expense1 = Expense.create!([ {
+  trip_id: '1',
+  user_id: '1',
+  amount: '30',
+  description: 'Wine'
+  }])
+
+  expense2 = Expense.create!([ {
+  trip_id: '1',
+  user_id: '2',
+  amount: '50',
+  description: 'Hmaburgers'
+  }])
+
+  expense3 = Expense.create!([ {
+  trip_id: '1',
+  user_id: '1',
+  amount: '200',
+  description: 'Fries'
+  }])
+
+  expense4 = Expense.create!([ {
+  trip_id: '2',
+  user_id: '1',
+  amount: '300',
+  description: 'Vodka'
+  }])
+
+  expense5 = Expense.create!([ {
+  trip_id: '2',
+  user_id: '2',
+  amount: '50',
+  description: 'Teddy bears'
+  }])
+
+  expense6 = Expense.create!([ {
+  trip_id: '2',
+  user_id: '2',
+  amount: '200',
+  description: 'Skis'
+  }])
+
+  expense7 = Expense.create!([ {
+  trip_id: '3',
+  user_id: '3',
+  amount: '30',
+  description: 'Tent'
+  }])
+
+  expense8 = Expense.create!([ {
+  trip_id: '3',
+  user_id: '3',
+  amount: '50',
+  description: 'Juice'
+  }])
+
+  expense9 = Expense.create!([ {
+  trip_id: '3',
+  user_id: '3',
+  amount: '200',
+  description: 'Mosquito Spray'
+  }])
+
+  payee1 = Payee.create!([ { user_id: 1, expense_id: 1 }])
+  payee2 = Payee.create!([ { user_id: 2, expense_id: 1 }])
+  payee3 = Payee.create!([ { user_id: 3, expense_id: 1 }])
+  payee4 = Payee.create!([ { user_id: 1, expense_id: 2 }])
+  payee5 = Payee.create!([ { user_id: 2, expense_id: 2 }])
+  payee6 = Payee.create!([ { user_id: 1, expense_id: 3 }])
+  payee7 = Payee.create!([ { user_id: 1, expense_id: 4 }])
+  payee8 = Payee.create!([ { user_id: 1, expense_id: 5 }])
+  payee9 = Payee.create!([ { user_id: 2, expense_id: 5 }])
+  payee10 = Payee.create!([ { user_id: 2, expense_id: 6 }])
+  payee11 = Payee.create!([ { user_id: 3, expense_id: 7 }])
+  payee12 = Payee.create!([ { user_id: 3, expense_id: 8 }])
+  payee13 = Payee.create!([ { user_id: 3, expense_id: 8 }])
+  payee14 = Payee.create!([ { user_id: 2, expense_id: 9 }])
