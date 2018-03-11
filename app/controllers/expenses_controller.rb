96293a09668bbfb48d6884bc1e2982ab550aee75
Laura De Witt <lauramdewitt@gmail.com>
2018-03-11 19:36:13 +0000

    Added expense functionality by amount:
    -adjusted expense options form to submit values for expense by payee name and portion of amount
    -added logic in expense controller to check if hidden split type by amount exists, and update balances of payees with entered portions/amounts.
    
    TODO: check if total of portions matches the entered total amount. If not, should display error to user.

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -26,31 +26,43 @@ class ExpensesController < ApplicationController
     @expense = Expense.new(expense_params)
     @expense.trip_id = params[:trip_id]
     @trip = Trip.find(params[:trip_id].to_i)
-    @willing_payees = params[:expense][:payee][:user_id].select { |uid| uid.length > 0 }
-    @willing_payees.each do |pid|
-      @expense.payees.new(user_id: pid)
-      # TODO: this is kinda cavalier about the possibility of errors.  ha ha!
+    # create hash to store split portions for each user
+    split_hash = Hash.new()
+    if params[:split_type] == "split_by_amount"
+      passed_split = params[:expense][:payee][:split]
+      passed_split.each do |id, obj|
+        @expense.payees.new(user_id: id)
+        split_hash[id] = Money.new(obj[:portion].to_i * 100, 'USD')
+      end
+    else
+      @willing_payees = params[:expense][:payee][:user_id].select { |uid| uid.length > 0 }
+      @amount = @expense.amount
+      @payee_size = @willing_payees.size
+      @willing_payees.each do |pid|
+        @expense.payees.new(user_id: pid)
+        split_hash[pid] = (@amount / @payee_size) #/
+
+        # TODO: this is kinda cavalier about the possibility of errors.  ha ha!
+      end
     end
-    @amount = @expense.amount
-    
-    @payee_size = @willing_payees.size
-    @payee_owes = (@amount / @payee_size)
-    
-    @willing_payees.each do |payee|
-      @user_id = payee
+
+    # go through hash and calculate user's owed balance
+    split_hash.each do |key, value|
+      @user_id = key
       @payee_user = User.where(id: @user_id)
       @payee_user_ids = @payee_user.ids
       @attendee_user_for_finding_expense = Attendee.where(user_id: @payee_user_ids, trip_id: params[:trip_id].to_i)
       @attendee_user_for_finding_expense.each do |attendee_for_balance|
-        attendee_for_balance.balance += @payee_owes
+        attendee_for_balance.balance += value
         @attendee_balance = attendee_for_balance.balance
         attendee_for_balance.update_attribute(:balance, @attendee_balance)
       end
     end
+
+    # deduct amount paid by expense user
     @attendee_payer_of_expense = Attendee.where(user_id: @expense.user_id, trip_id: params[:trip_id].to_i).first
     @payer_balance = @attendee_payer_of_expense.balance - @expense.amount
     @attendee_payer_of_expense.update_attribute(:balance, @payer_balance)
-    
 
     respond_to do |format|
       if @expense.save
@@ -94,10 +106,10 @@ class ExpensesController < ApplicationController
       @willing_payees = @expense.payees.all
       # TODO: this is kinda cavalier about the possibility of errors.  ha ha!
       @amount = @expense.amount
-    
+
       @payee_size = @willing_payees.size
-      @payee_owes = (@amount / @payee_size)
-    
+      @payee_owes = (@amount / @payee_size) #/
+
       @willing_payees.each do |payee|
         @user_id = payee.user_id
         @payee_user = User.where(id: @user_id)
