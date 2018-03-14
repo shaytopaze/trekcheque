79e54e20e3c2145ad0f8da818be6cc6a467e09b3
Stephanie Simpson <stephsimpson911@gmail.com>
2018-03-14 21:11:00 +0000

    added some maximums for names/descriptions and did word wraps. Also made it so you couldn't join if there were as many attendees as max people

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -66,7 +66,7 @@ class TripsController < ApplicationController
                 negative[1] = negative[1] - @temp
                 @user_who_owes = User.find(positive[0])
                 @user_getting_paid = User.find(negative[0])
-                @owe_statements.push("#{@user_who_owes.name} owes #{@user_getting_paid.name} #{@temp}")
+                @owe_statements.push("#{@user_who_owes.name} owes #{@user_getting_paid.name} $#{@temp}")
               end
             end
           end
