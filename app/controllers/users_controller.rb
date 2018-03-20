6fd2c981eda813ef95494082b2a39437f8cc9146
Stephanie Simpson <stephsimpson911@gmail.com>
2018-03-20 18:09:59 +0000

    clean up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,7 @@ class UsersController < ApplicationController
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
+    
   end
 
   # PATCH/PUT /users/1
