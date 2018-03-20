7329dd99de16c08045a5405e584b90132b28df8e
Laura De Witt <lauramdewitt@gmail.com>
2018-03-20 05:35:47 +0000

    styled chat, need to fix join trip feature

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -34,6 +34,13 @@ class AttendeesController < ApplicationController
     end
     @trips = Trip.all
     @attendee = Attendee.new(attendee_params)
+    puts
+    puts
+    puts
+    puts
+    puts
+    puts 'params'
+    puts attendee_params
     @attendee.trip_id = params[:trip_id]
     @attendee.user_id = current_user.id
     @attendee.balance = 0
@@ -41,7 +48,12 @@ class AttendeesController < ApplicationController
     @trip_length_night = (@trip.end_date - @trip.start_date).to_i
     @price_per_night = @trip.price_per_night
     @total_cost = @price_per_night.to_i * @trip_length_night.to_i
-  
+    puts
+    puts
+    puts
+    puts
+    puts 'attendee ======='
+    puts @attendee.inspect()
     if @attendee.save
       @total_confirmed_accomodation_cost_per_person = @total_cost.to_i / @attendees.size
       @trip.update_attribute(:total_confirmed_cost, @total_confirmed_accomodation_cost_per_person)
